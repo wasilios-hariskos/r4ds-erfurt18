8b105ada50112c7c727c314f7c7106c0b5eb9a6f
Wasilios Hariskos <hariskos@ad.uni-erfurt.de>
2018-10-18 11:21:53 +0200

    Vorbereitung Lehre 18.10.2018.

---
+++ 01-IntroR.R
@@ -0,0 +1,49 @@
+# Basics
+
+## Zuweisung einer Variablen
+
+## Rechenoperationen
+
+## Datentypen
+
+# Vektoren
+
+## Erstellen
+
+## Benennen
+
+## Addition
+
+## Summieren 
+
+## Auswählen
+
+## Auswählen durch Vergleich
+
+# Matrix
+
+## Erstellen
+
+## Benennen
+
+## Summieren von Zeilen
+
+## Summieren von Spalten
+
+## Auswählen
+
+## Rechenoperationen
+
+# Faktoren
+
+## Erstellen
+
+## Nominalskalierte kategoriale Variable (unordered factor)
+
+## Ordinalskalierte kategoriale Variable (ordered factor)
+
+## Änderung des Faktorlevels
+
+## Kurzdarstellung
+
+  
